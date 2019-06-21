013217d338250e6fb37ed5566cb440ab6bb3a770
Farhaan900 <mfarhaan.shaikh95@gmail.com>
2019-06-21 23:31:33 +0200

    agent and scores

---
+++ Agent/MyAgent.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SabberStoneCore.Tasks;
+using SabberStoneCoreAi.Agent;
+using SabberStoneCoreAi.POGame;
+using SabberStoneCore.Tasks.PlayerTasks;
+using System.IO;
+using CsvHelper;
+using System;
+using System.Linq;
+using SabberStoneCore.Enums;
+using SabberStoneCore.Tasks;
+using SabberStoneCoreAi.Score;
+using SabberStoneCore.Tasks.PlayerTasks;
+
+
+namespace SabberStoneCoreAi.Agent
+{
+	class MyAgent : AbstractAgent
+	{
+		private Random Rnd = new Random();
+
+		public override void FinalizeAgent()
+		{
+		}
+
+		public override void FinalizeGame(int marker )
+		{
+		}
+
+		public override PlayerTask GetMove(SabberStoneCoreAi.POGame.POGame poGame)
+		{
+			List<PlayerTask> simulatedactions = new List<PlayerTask>();
+			simulatedactions.AddRange(poGame.CurrentPlayer.Options());
+			Dictionary<PlayerTask, SabberStoneCoreAi.POGame.POGame> sim = poGame.Simulate(simulatedactions);
+
+			Dictionary<PlayerTask, SabberStoneCoreAi.POGame.POGame>.KeyCollection keyColl = sim.Keys;
+
+			foreach (PlayerTask key in keyColl)
+			{
+
+
+				//do something with simulated actions
+				//in case an EndTurn was simulated you need to set your own cards
+				//see POGame.prepareOpponent() for an example
+
+				//Console.WriteLine(key);
+
+			}
+
+			Dictionary<int, PlayerTask> scoresKeyPair = new Dictionary<int, PlayerTask>();
+			scoresKeyPair.Clear();
+
+			int maxScore = Int32.MinValue;
+			int _score = 0;
+
+			foreach (PlayerTask key in keyColl)
+			{
+				//Console.WriteLine(key);
+				//Console.WriteLine("Player num -->>>>"+poGame.CurrentPlayer.PlayerId);
+				//Console.WriteLine("SIM  -->>>>"+sim[key]);
+
+
+				if (sim[key] == null)
+					continue;
+
+				if (key.PlayerTaskType == PlayerTaskType.END_TURN)
+				{
+					_score = Int32.MinValue + 1;
+				}
+
+				else
+					_score = Score(sim[key], poGame.CurrentPlayer.PlayerId);
+				//Console.WriteLine(_score);
+				if (!scoresKeyPair.ContainsKey(_score))
+					scoresKeyPair.Add(_score, key);
+				if (_score > maxScore)
+					maxScore = _score;
+
+			}
+
+			//Console.WriteLine("Played ==>> "+scoresKeyPair[maxScore]);
+			return scoresKeyPair[maxScore];
+			return poGame.CurrentPlayer.Options()[0];
+		}
+
+		private static int Score(POGame.POGame state, int playerId)
+		{
+			var p = state.CurrentPlayer.PlayerId == playerId ? state.CurrentPlayer : state.CurrentOpponent;
+			switch (state.CurrentPlayer.HeroClass)
+			{
+				case CardClass.MAGE: return new MohammedsMageMontage { Controller = p }.Rate();
+				case CardClass.WARRIOR: return new MohammedsWarriorrsWhip { Controller = p }.Rate();
+				case CardClass.SHAMAN: return new MohammedsShamanKiller { Controller = p }.Rate();
+				case CardClass.HUNTER: return new MohammedsHunterHonor { Controller = p }.Rate();
+				case CardClass.ROGUE: return new MohammedsGlobalDominator { Controller = p }.Rate();
+				default: return new MohammedsGlobalDominator { Controller = p }.Rate();
+			}
+		}
+
+		public override void InitializeAgent()
+		{
+			Rnd = new Random();
+		}
+
+		public override void InitializeGame()
+		{
+		}
+	}
+}
---
+++ Score/MohammedsHunterHonor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using SabberStoneCore.Model.Entities;
+
+namespace SabberStoneCoreAi.Score
+{
+	public class MohammedsHunterHonor : Score
+	{
+		public override int Rate()
+		{
+			if (OpHeroHp < 1)
+				return Int32.MaxValue;
+
+			if (HeroHp < 1)
+				return Int32.MinValue;
+
+			int result = 0;
+
+			if (OpBoardZone.Count == 0 && BoardZone.Count > 0)
+				//result += 1000;
+				result += 400;
+			//else
+			result += (BoardZone.Count - OpBoardZone.Count) * 50;
+
+			//if (OpMinionTotHealthTaunt > 0)
+			//	result += OpMinionTotHealthTaunt * -1000;
+
+			result += (MinionTotHealthTaunt - OpMinionTotHealthTaunt) * 600;
+
+			result += (MinionTotHealth - OpMinionTotHealth) * 250;
+
+			result += (MinionTotAtk - OpMinionTotAtk) * 450;
+
+			result += (HeroHp - OpHeroHp) * 310;
+
+			result += HeroAtk * 910;
+
+			result += (HandCnt - OpHandCnt) * 20;
+			/**
+			if (DeckCnt == 0)
+				result -= 700;
+
+			if (OpDeckCnt == 0)
+				result += 900;
+	**/
+			return result;
+		}
+
+		public override Func<List<IPlayable>, List<int>> MulliganRule()
+		{
+			return p => p.Where(t => t.Cost > 3).Select(t => t.Id).ToList();
+		}
+	}
+}
